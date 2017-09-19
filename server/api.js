40a243c3f476e5f9325e1b822df2a67c7f5ad7d7
Sudara Fernando <sudara@yahoo.com>
2017-09-19 20:13:19 +1200

    Fixed API broken paths

--- server/api.js
+++ server/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //Test path to check if the Server endpoint is responding
-router.get('test', (req, res) => {
+router.get('/test', (req, res) => {
   res.status(200);
   res.json({
     "success" : "Server API endpoint is working successfully"
